b9d1f46ee05d63ab10ce460a6cfa95a091c13bab
Austin Faulkner <faulkner.austin@gmail.com>
2022-03-03 09:10:45 -0600

    Update ExploreDataManager.swift

--- AustinDrinks/Explore/Model/ExploreDataManager.swift
+++ AustinDrinks/Explore/Model/ExploreDataManager.swift
@@ -2,7 +2,7 @@
 //  ExploreDataManager.swift
 //  AustinDrinks
 //
-//  Created by Austin Faulkner on 1/12/22.
+//  Created by Austin Faulkner on 1/31/22.
 //
 
 import Foundation  // one of Apple's core frameworks. To read more about it visit https://developer.apple.com/documentation/foundation
