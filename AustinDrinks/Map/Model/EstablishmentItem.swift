c18777f241879e84d5c2d092c976c021832db7a2
Austin Faulkner <faulkner.austin@gmail.com>
2022-03-03 09:20:38 -0600

    Update EstablishmentItem.swift

--- AustinDrinks/Map/Model/EstablishmentItem.swift
+++ AustinDrinks/Map/Model/EstablishmentItem.swift
@@ -2,37 +2,14 @@
 //  EstablishmentItem.swift
 //  AustinDrinks
 //
-//  Created by Austin Faulkner on 1/13/22.
+//  Created by Austin Faulkner on 2/22/22.
 //
 
 import UIKit
 import MapKit
 
 class EstablishmentItem: NSObject, MKAnnotation, Decodable {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // For Boston Test:
-//    let id: Int?
-//    let name: String?
-//    let address: String?
-//    let city: String?
-//    let state: String?
-//    let area: String?
-//    let postal_code: String?
-//    let country: String?
-//    let phone: String?
-//    let lat: Double?
-//    let long: Double?
-//    let price: Int?
-//    let cuisines: [String]
-//    let reserve_url: String?
-//    let modile_reserve_url: String?
-//    let image_url: String?
-    
-=======
->>>>>>> d5fa275c63f97a2a1678d183395d3c5da05b7d6e
-=======
->>>>>>> d5fa275c63f97a2a1678d183395d3c5da05b7d6e
+
     let name: String?
     let open_for_business: Int?
     let tags: [String]
@@ -62,26 +39,7 @@ class EstablishmentItem: NSObject, MKAnnotation, Decodable {
     let rating_count: Int?
     let reviews: [[String: String]]
 
-    // For Boston Test:
-//    enum CodingKeys: String, CodingKey {
-//        case id
-//        case name
-//        case address
-//        case city
-//        case state
-//        case area
-//        case postal_code
-//        case country
-//        case phone
-//        case lat
-//        case long
-//        case price
-//        case cuisines
-//        case reserve_url
-//        case modile_reserve_url
-//        case image_url
-//    }
-    
+   
     enum CodingKeys: String, CodingKey {
         case name = "name"
         case open_for_business = "open_for_business"
@@ -134,21 +92,6 @@ class EstablishmentItem: NSObject, MKAnnotation, Decodable {
 =======
 >>>>>>> d5fa275c63f97a2a1678d183395d3c5da05b7d6e
     }
-
-    
-    // RUNNING THIS BUILD WILL PRODUCE AN ERROR IN THE fetch(completion:) METHOD IN THE MapDataManager FILE. NEXT SECTION WILL FIX THIS ISSUE. JAnuary 18, 2022, 8:07 pm
-    
-//    init(dict: [String: AnyObject]) {
-//        self.lat = dict["lat"] as? Double
-//        self.long = dict["long"] as? Double
-//        self.name = dict["name"] as? String
-//        self.cuisines = dict["cuisines"] as? [String] ?? []
-//        self.address = dict["address"] as? String
-//        self.postalCode = dict["postalCode"] as? String
-//        self.state = dict["state"] as? String
-//        self.imageURL = dict["image_url"] as? String
-//        self.restaurantID = dict["id"] as? Int
-//    }
     
     var coordinate: CLLocationCoordinate2D {
         guard let latitude = latitude, let longitude = longitude else {
@@ -180,19 +123,5 @@ class EstablishmentItem: NSObject, MKAnnotation, Decodable {
         } else {
             return tags.joined(separator: ", ")
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-//        if cuisines.isEmpty {
-//            return ""
-//        } else if cuisines.count == 1 {
-//            return cuisines.first
-//        } else {
-//            return cuisines.joined(separator: ", ")
-//        }
-=======
->>>>>>> d5fa275c63f97a2a1678d183395d3c5da05b7d6e
-=======
->>>>>>> d5fa275c63f97a2a1678d183395d3c5da05b7d6e
     }
 }
